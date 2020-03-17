6254b945721c47246088b8117402f38ddcffbdc3
Cao Thu <thanhthu0321@gmail.com>
2020-03-17 13:28:02 +0700

    Day01

---
+++ Day1_MyMusic/app/src/main/java/thu/ct/day1_mymusic/MainActivity.java
@@ -0,0 +1,34 @@
+package thu.ct.day1_mymusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnPlay, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnPlay=findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyMusicService.class);
+                startService(intent);
+            }
+        });
+        btnStop=findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyMusicService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
